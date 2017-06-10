3409fda7ab742bd3aadf9fa8ae3498f2b00cd36a
RenanNascimento <renanmateusbn@gmail.com>
2017-06-10 15:47:55 -0300

    app runserver deletado

--- integrador/settings.py
+++ integrador/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'src'
+    'src',
+    'selecionar_materias',
+    'classificar_materias',
 ]
 
 MIDDLEWARE = [
