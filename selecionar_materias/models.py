1dc916da4cc899d8b90af127b1a471613e9e5c99
Vinicius Magalhaes <viniciusvmda@gmail.com>
2017-06-10 19:09:04 -0300

    Adicionado models Caderno e Materia

--- selecionar_materias/models.py
+++ selecionar_materias/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Caderno(models.Model):
+    descricao = models.CharField(max_length=200)
+
+class Materia(models.Model):
+    titulo = models.CharField(max_length=90)
+    caderno= models.ForeignKey('Caderno', on_delete=models.SET_NULL, related_name="caderno", null=True, blank=True)
+    data_importacao = models.DateField(auto_now_add=True)
+    publicada = models.BooleanField()
+
+    def __str__(self):
+        return str("Título: " + str(self.titulo))
